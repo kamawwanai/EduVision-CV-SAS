c77eeae4b25907ed56b78ec34539d6ad742b9a8b
Ksenia Karimova <karimovaksusa@gmail.com>
2024-06-13 00:21:55 +0300

    new build and users db

--- sources/main.cpp
+++ sources/main.cpp
@@ -191,35 +191,7 @@ void recognize_faces(cv::CascadeClassifier& face_cascade, anet_type& net, dlib::
     }
 }
 
-
-struct User{
-    int id;
-    std::string firstName;
-    std::string lastName;
-    int birthDate;
-    std::unique_ptr<std::string> imageUrl;
-    int typeId;
-};
-
-struct UserType {
-    int id;
-    std::string name;
-};
-
-
 auto main() -> int {
-    using namespace sqlite_orm; // NOLINT
-    auto storage = make_storage("db.sqlite",
-                                make_table("users",
-                                        make_column("id", &User::id, primary_key().autoincrement()),
-                                        make_column("first_name", &User::firstName),
-                                        make_column("last_name", &User::lastName),
-                                        make_column("birth_date", &User::birthDate),
-                                        make_column("image_url", &User::imageUrl),
-                                        make_column("type_id", &User::typeId)),
-                                make_table("user_types",
-                                        make_column("id", &UserType::id, primary_key().autoincrement()),
-                                        make_column("name", &UserType::name, default_value("name_placeholder"))));
     // train_model();
     cv::VideoCapture cap(0, cv::CAP_DSHOW);
     if (!cap.isOpened()) {
